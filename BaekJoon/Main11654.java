6ebb77e2efce7c37c37e6e80883a836e2e7314fc
ShinjaeKwon <80821744+ShinjaeKwon@users.noreply.github.com>
2021-07-10 23:51:53 +0900

    JavaAlgorithm
    백준알고리즘 11654

---
+++ BaekJoon/Main11654.java
@@ -0,0 +1,13 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main11654 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		String s = bf.readLine();
+		int ch = s.charAt(0);
+		System.out.println(ch);
+	}
+
+}
