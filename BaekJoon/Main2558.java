a0c6bd7c5f0794b7dd97b663a9750d95e9ae11f1
JaeKwonShin <sjk6437@gmail.com>
2021-08-04 00:40:30 +0900

    백준 2558

---
+++ BaekJoon/Main2558.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+
+public class Main2558 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		int A = Integer.parseInt(bf.readLine());
+		int B = Integer.parseInt(bf.readLine());
+		
+		System.out.println(A+B);
+		
+	
+	}
+
+}
