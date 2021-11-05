41cfe7a67a50ceb2b1f82c6214d7293a09998df8
JaeKwonShin <sjk6437@gmail.com>
2021-11-05 22:52:23 +0900

    재귀함수(무한루프)

---
+++ AlgorithmPractice/DFS/BFS_DFS0503.java
@@ -0,0 +1,15 @@
+package CodeTest;
+
+public class BFS_DFS0503 {
+
+	public static void main(String[] args) {
+		recursiveFunction();
+
+	}
+	
+	public static void recursiveFunction(){
+		System.out.println("재귀 함수를 호출합니다.");
+		recursiveFunction();
+	}
+
+}
