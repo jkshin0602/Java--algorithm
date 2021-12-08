ad6ac48545cc7a992012085e3f838790d1e731e1
ShinjaeKwon <sjk6437@gmail.com>
2021-12-09 00:33:31 +0900

    백준 15651 (수정)

--- BaekJoon/Main15651.java
+++ BaekJoon/Main15651.java
@@ -3,44 +3,46 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-
 public class Main15651 {
 	
-	public static int N, M;
-	public static int[] selected; 
-	public static StringBuilder sb = new StringBuilder();
+	static int N;
+	static int M;
 	
+	static int[] arr = new int[10];
+	
+	static StringBuilder sb = new StringBuilder();
 
-	public static void main(String[] args) throws IOException{
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		N = Integer.parseInt(st.nextToken()); // 1~N 까지4
-		M = Integer.parseInt(st.nextToken()); // M개를 고른 수열 
-		selected = new int[M+1]; //결과 수열
-		rec_func(1);
-		System.out.println(sb.toString());
 		
+		N = Integer.parseInt(st.nextToken()); //1부터 M까지 자연수 중에서 
+		M = Integer.parseInt(st.nextToken()); // M개를 고른 수 (중복 허용)
 		
+
+		func(0);
+		
+		System.out.println(sb); 	
+		
+
 	}
 	
-	public static void rec_func(int k){
-		if(k == M + 1){ //다 골랐을때
-			//selected[1...M] 배열이 새롭게 탐색된 결과
-			for(int i=1;i <=M; i++) {
-				sb.append(selected[i]).append(' ');
+	static void func(int k) { //k개까지 수를 택했다
+		if(k == M) { // M개까지의 수를 택했으면 출력 
+			for(int i=0; i<M; i++) {
+				sb.append(arr[i]).append(' ');
 			}
 			sb.append('\n');
-		}else{
-			// 1~N 까지를 k번 원소로 한번씩 정하고
-			// 매번 k+1 번부터 M번 원소로 재귀호출 해주기
-			for(int cand = 1; cand <= N; cand++){
-				selected[k] = cand;
-				//k+1 번 ~ M번을 모두 탐색하는 일을 해야하는 상황
-				rec_func(k+1);
-				selected[k] = 0; 
-			}
+			return;
+		}
+		
+		for(int i=1; i<=N; i++) { //1부터 n까지의 수에 대해
+			arr[k] = i; //k번째를 i로 정한다. 
+			func(k+1); 
+			
 		}
 	}
+	
 	
 	
 
