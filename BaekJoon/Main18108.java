b3ee9e736bf10b5b025e051cb1ca924b0411a40d
ShinjaeKwon <sjk6437@gmail.com>
2022-03-10 00:18:38 +0900

    백준 18108

---
+++ BaekJoon/Main18108.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main18108 {
+
+	public static void main(String[] args) throws IOException {
+		 BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	        int y = Integer.parseInt(br.readLine());
+	        int c = 2541-1998;
+	        
+	        System.out.println(y-c);	
+		
+		
+	}
+
+	
+}
