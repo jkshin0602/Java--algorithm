2f74882328dfd003e8943d622efcecddab7b9c08
ShinjaeKwon <sjk6437@gmail.com>
2022-06-21 13:27:17 +0900

    백준 1012

--- BaekJoon/Main1012.java
+++ BaekJoon/Main1012.java
@@ -1,69 +1,93 @@
+package baekjoon.DFS와BFS;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main1012 {
-	
-	public static int[] dy = {-1,0,1,0};
-	public static int[] dx = {0,1,0,-1};
-	public static int[][] map;
-	public static boolean[][] check;
-	public static int M, N;	
-	
-	public static void main(String[] args) throws IOException {
-		
+
+	static int T, M, N, K;
+	static int[][] map;
+	static boolean[][] visited;
+	static int[] dy = {-1, 0, 1, 0};
+	static int[] dx = {0, 1, 0, -1};
+	static Queue<Pair> q = new LinkedList<>();
+
+	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-		
-		//인접한, 최소 갯수 -> DFS사용 
-		int T = Integer.parseInt(br.readLine()); //테스트케이스 T
-		while(T-- > 0) {
-			st = new StringTokenizer(br.readLine()); 
-			M = Integer.parseInt(st.nextToken()); //가로의 길이 M
-			N = Integer.parseInt(st.nextToken()); //세로의 길이 N
-			int K = Integer.parseInt(st.nextToken()); //배추심은 갯수 K
-			map = new int[N][M];
-			check = new boolean[N][M];
-			
-			for(int i=0; i<K; i++) {
-				st = new StringTokenizer(br.readLine()); 
-				int x = Integer.parseInt(st.nextToken());
-				int y = Integer.parseInt(st.nextToken());
-				map[y][x] = 1;	
-			}
-			
+		T = Integer.parseInt(br.readLine());
+		while (T-- > 0) {
+			input(br);
 			int ans = 0;
-			for(int i=0; i<N; i++) {
-				for(int j=0; j<M; j++) {
-					if(map[i][j] == 1 && !check[i][j]) {
-						dfs(i,j);
+			for (int i = 0; i < N; i++) {
+				for (int j = 0; j < M; j++) {
+					if (map[i][j] == 1 && !visited[i][j]) {
+						BFS(i, j);
 						ans++;
 					}
 				}
- 			}
-			
+			}
 			System.out.println(ans);
-		
 		}
-		
 	}
-	
-	public static void dfs(int y, int x) {
-		check[y][x] = true;
-		for(int i=0; i<4; i++) {
-			int ny = y + dy[i];
-			int nx = x + dx[i];
-			if(ny < 0 || nx < 0 || ny>=N || nx>=M) continue;
-			if(map[ny][nx] == 1 && !check[ny][nx]) {
-				dfs(ny, nx);
+
+	private static void BFS(int y, int x) {
+		q.add(new Pair(y, x));
+		visited[y][x] = true;
+		while (!q.isEmpty()) {
+			Pair p = q.poll();
+			for (int i = 0; i < 4; i++) {
+				int ny = p.y + dy[i];
+				int nx = p.x + dx[i];
+				if (isMap(ny, nx))
+					continue;
+				q.add(new Pair(ny, nx));
+				visited[ny][nx] = true;
 			}
 		}
-		return;
+
 	}
-	
-	
-	
+
+	private static void DFS(int y, int x) {
+		visited[y][x] = true;
+		for (int i = 0; i < 4; i++) {
+			int ny = y + dy[i];
+			int nx = x + dx[i];
+			if (isMap(ny, nx))
+				continue;
+			DFS(ny, nx);
+		}
+	}
+
+	private static boolean isMap(int ny, int nx) {
+		return ny < 0 || nx < 0 || ny >= N || nx >= M || visited[ny][nx] || map[ny][nx] == 0;
+	}
+
+	private static void input(BufferedReader br) throws IOException {
+		StringTokenizer st;
+		st = new StringTokenizer(br.readLine());
+		M = Integer.parseInt(st.nextToken());
+		N = Integer.parseInt(st.nextToken());
+		K = Integer.parseInt(st.nextToken());
+		map = new int[N][M];
+		visited = new boolean[N][M];
+		for (int i = 0; i < K; i++) {
+			st = new StringTokenizer(br.readLine());
+			int x = Integer.parseInt(st.nextToken());
+			int y = Integer.parseInt(st.nextToken());
+			map[y][x] = 1;
+		}
+	}
+
+	private static class Pair {
+		int y, x;
+
+		public Pair(int y, int x) {
+			this.y = y;
+			this.x = x;
+		}
+	}
 }
