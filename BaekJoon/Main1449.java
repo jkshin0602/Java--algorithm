3bc158e2cb5bf70e91d19beb0624fac6ae309f18
JaeKwon <sjk6437@gmail.com>
2022-07-20 01:22:58 +0900

    백준 1449

--- BaekJoon/Main1449.java
+++ BaekJoon/Main1449.java
@@ -1,53 +1,42 @@
-package Cho.week_3;
+package baekjoon.그리디;
+
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
+class Main1449 {
 
-public class Main1449 {
-
-	public static void main(String[] args) throws IOException{
+	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		int N = Integer.parseInt(st.nextToken()); // 물 새는 곳의 개수 N
-		int L = Integer.parseInt(st.nextToken()); // 테이프의 길이 L
-		int[] water = new int[N]; //물 새는 위치
-		
+		int N = Integer.parseInt(st.nextToken());
+		int L = Integer.parseInt(st.nextToken());
+		int[] a = new int[N];
 		st = new StringTokenizer(br.readLine());
-		for(int i=0; i<N; i++){
-			water[i] = Integer.parseInt(st.nextToken()); //물이 새는 위치
+		for (int i = 0; i < N; i++) {
+			a[i] = Integer.parseInt(st.nextToken());
 		}
-		Arrays.sort(water);
-		
-		// 테이프를 붙일 때 좌우 0.5씩 최소 1cm를 붙이고, 테이프는 겹칠수 있다.
-		//1. 물이 새는 두 곳의 길이 + 1이 테이프 길이보다 작을 때 ->다음 구멍까지 검사
-		//2. 물이 새는 두 곳의 길이 + 1이 테이프 길이보다 클 때 -> 그 구멍까지 검사 
-		int cnt = 0; 
-		
-		for(int i=0; i<N; i++){
-			if(i == N-1){ //마지막 원소
-				cnt++;
-				break; 
+
+		int ans = 0;
+		Arrays.sort(a);
+		for (int i = 0; i < N; i++) {
+			if (i == N - 1) {
+				ans++;
+				break;
 			}
-			
-			int idx = water[i++];
-			while(N > i){
-				if(water[i] - idx +1 <= L){ //1번 경우
-					i++; //다음 구멍까지 커버되는지 검사
-				}else{ //2번 경우
+
+			int tmp = a[i++];
+			while (N > i) { //테이프길이 안에 들어오는만큼
+				if (a[i] - tmp + 1 <= L) {
+					i++;
+				} else {
+					i--;
 					break;
 				}
-			}  //여기 반복문을 빠져 나온것은 테이프길이가 부족한것이다.
-			cnt++;
-			i--;  //이전것과의 차이를 검사하기 위해 i를 1줄여준다.
-			
-			
+			}
+			ans++;
 		}
-		System.out.println(cnt);
-		
-		
+		System.out.println(ans);
 	}
-
 }
