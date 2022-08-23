bfd3692735c72917f62f7b0e03f222234f898b44
sjk <80821744+ShinjaeKwon@users.noreply.github.com>
2022-08-23 15:51:44 +0900

    백준 11719

---
+++ BaekJoon/Main11719.java
@@ -0,0 +1,15 @@
+package baekjoon.구현;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+class Main11719 {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String str = "";
+		while ((str = br.readLine()) != null) {
+			System.out.println(str);
+		}
+	}
+}
