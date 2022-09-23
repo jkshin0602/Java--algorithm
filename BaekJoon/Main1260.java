00475a232262fedd6e1927523ccef0713f89324d
sjk <80821744+ShinjaeKwon@users.noreply.github.com>
2022-09-24 00:10:03 +0900

    백준 1260

--- BaekJoon/Main1260.java
+++ BaekJoon/Main1260.java
@@ -1,3 +1,5 @@
+package baekjoon.DFS와BFS;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -7,82 +9,86 @@ import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
+class Main1260 {
 
-public class Main1260 {
-	
-	public static ArrayList<Integer>[] graph;
-	public static int N;
-	public static boolean[] visited; //방문
+	static StringBuilder sb = new StringBuilder();
+	static int N, M, V;
+	static ArrayList<Integer>[] graph;
+	static Queue<Integer> q;
+	static boolean[] visited;
 
-	//dfs
-	public static void dfs(int V){
-		//현재 노드를 방문 처리
-		visited[V] = true;
-		System.out.print(V+" ");
-		//현재 노드와 연결된 다른 노드를 재귀적으로 방문
-		for(int y : graph[V]){
-			if(!visited[y]) 
-				dfs(y);
- 		}
+	public static void main(String[] args) throws Exception {
+		input();
+		go();
+		System.out.println(sb);
 	}
-	
-	//bfs 
-	public static void bfs(int V){
-		Queue<Integer> q = new LinkedList<Integer>();
-		q.add(V);
-		//현재 노드를 방문 처리
-		visited[V] = true;
-		//큐가 빌때까지 반복
-		while(!q.isEmpty()){
-			//큐에서 하나의 원소를 뽑아 출력
-			int x = q.poll();
-			System.out.print(x + " ");
-			//해당 원소와 연결된, 아직 방문하지 않는 원소들을 큐에 삽입
-			for(int y : graph[x]){
-				if(!visited[y]){
-					visited[y] = true;
-					q.add(y);
-				}
+
+	private static void go() {
+		dfs(V);
+		sb.append('\n');
+		init();
+		bfs(V);
+	}
+
+	private static void init() {
+		for (int i = 1; i <= N; i++) {
+			visited[i] = false;
+		}
+	}
+
+	private static void dfs(int start) {
+		visited[start] = true;
+		sb.append(start).append(' ');
+
+		for (Integer a : graph[start]) {
+			if (visited[a])
+				continue;
+			dfs(a);
+		}
+	}
+
+	private static void bfs(int start) {
+		Queue<Integer> q = new LinkedList<>();
+
+		q.add(start);
+		visited[start] = true;
+
+		while (!q.isEmpty()) {
+			Integer a = q.poll();
+			sb.append(a).append(' ');
+
+			for (Integer b : graph[a]) {
+				if (visited[b])
+					continue;
+				q.add(b);
+				visited[b] = true;
 			}
 		}
-
 	}
 
-	public static void main(String[] args) throws IOException{
+	private static void input() throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		N = Integer.parseInt(st.nextToken()); // 정점의 개수
-		int M = Integer.parseInt(st.nextToken()); // 간선의 개수
-		int V = Integer.parseInt(st.nextToken()); // 시작할 노드 번호
-		
-		graph = new ArrayList[N+1];
-		
-		//그래프 초기화
-		for(int i=1; i<N+1; i++){
-			graph[i] = new ArrayList<Integer>();
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		V = Integer.parseInt(st.nextToken());
+		graph = new ArrayList[N + 1];
+		visited = new boolean[N + 1];
+		for (int i = 1; i <= N; i++) {
+			graph[i] = new ArrayList<>();
 		}
-		//연결 그래프 생성
-		for(int i=0; i<M; i++){
+		for (int i = 0; i < M; i++) {
 			st = new StringTokenizer(br.readLine());
-			int x = Integer.parseInt(st.nextToken());
-			int y = Integer.parseInt(st.nextToken());
-			graph[x].add(y);
-			graph[y].add(x);
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			graph[a].add(b);
+			graph[b].add(a);
 		}
-		
-		for(int i=1; i<N+1; i++){
+		for (int i = 1; i <= N; i++) {
 			Collections.sort(graph[i]);
 		}
-		
-		visited = new boolean[N+1];
-		dfs(V);
-		System.out.println();
-		
-		visited = new boolean[N+1];
-		bfs(V);
-		
-		br.close();
-		
 	}
-
 }
+
+
+
