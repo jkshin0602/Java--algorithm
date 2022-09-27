15f831e4a10d1b0d8ce96c664573c9e2e9ece329
sjk <80821744+ShinjaeKwon@users.noreply.github.com>
2022-09-28 01:00:49 +0900

    백준 2805

--- BaekJoon/Main2805.java
+++ BaekJoon/Main2805.java
@@ -1,54 +1,61 @@
-package Cho.week_4;
+package baekjoon.이분탐색;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
+class Main2805 {
 
-public class Main2805 {
+	static int N, M;
+	static int[] h;
 
-	public static int N;
-	public static int M;
-	public static int[] tree;
-	public static int ans;
-	
-	public static void main(String[] args) throws IOException {
+
+
+	private static boolean determination(int H) {
+		long sum = 0;
+		for (int i = 1; i <= N; i++) {
+			if (h[i] > H) {
+				sum += h[i] - H;
+			}
+		}
+		return sum >= M;
+	}
+
+	private static void go() {
+		long L = 0, R = 2_000_000_000, ans = 0;
+
+		while (L <= R) {
+			long mid = (L + R) / 2;
+			if (determination((int)mid)) {
+				ans = mid;
+				L = mid + 1;
+			} else {
+				R = mid - 1;
+			}
+		}
+		System.out.println(ans);
+	}
+
+	public static void main(String[] args) throws Exception {
+		input();
+		go();
+	}
+
+	private static void input() throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		N = Integer.parseInt(st.nextToken()); //나무의 수 N
-		M = Integer.parseInt(st.nextToken()); //가져가려 하는 나무의 길이 M
-		
-		tree = new int[N];
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		h = new int[N + 1];
 		st = new StringTokenizer(br.readLine());
-		for(int i=0; i<N; i++){
-			tree[i] = Integer.parseInt(st.nextToken());
+		for (int i = 1; i <= N; i++) {
+			h[i] = Integer.parseInt(st.nextToken());
 		}
-		Arrays.sort(tree);
-		parametric_search();
-		System.out.println(ans);
-		
+		Arrays.sort(h);
 	}
-
-	public static void parametric_search(){
-		int left = 0;
-		int right = 1_000_000_000;
-		while(left <= right){
-			int mid = (left + right) / 2;
-			long sum = 0;
-			for(int i=0; i<N; i++){
-				if(tree[i] - mid > 0){ //나무 - mid 가 0보다 크면(자를 수 있는 것)
-					sum += tree[i] - mid;
-				}
-			}
-			if(sum >= M){ //sum >= M 이면, 높이를 올린다
-				ans = Math.max(ans, mid);
-				left = mid + 1;
-			}else{ //sum < M 이면, 높이를 낮춘다. (잘리는 개수가 더 많아짐)
-				right = mid - 1;
-			}
-		}
-	}
-	
-	
 }
+
+
+
