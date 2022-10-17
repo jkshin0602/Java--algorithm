a2cb77d9f7e6238f46b2bb23307a2b84fc29151a
sjk <80821744+ShinjaeKwon@users.noreply.github.com>
2022-10-17 11:32:48 +0900

    백준 2667

--- BaekJoon/Main2667.java
+++ BaekJoon/Main2667.java
@@ -1,7 +1,10 @@
+package baekjoon.DFS와BFS;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 //단지 번호 붙이기
@@ -15,8 +18,36 @@ public class Main2667 {
 	private static int[] dx = {0, 1, 0, -1};
 	private static int[][] map;
 	private static boolean[][] visited;
+	private static StringBuilder sb = new StringBuilder();
 
 	public static void main(String[] args) throws Exception {
+		input();
+		go();
+		print();
+	}
+
+	private static void print() {
+		Collections.sort(ans);
+		sb.append(ans.size()).append('\n');
+		for (Integer a : ans) {
+			sb.append(a).append('\n');
+		}
+		System.out.println(sb);
+	}
+
+	private static void go() {
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < N; j++) {
+				if (map[i][j] == 0 || visited[i][j])
+					continue;
+				cnt = 0;
+				DFS(i, j);
+				ans.add(cnt);
+			}
+		}
+	}
+
+	private static void input() throws IOException {
 		N = Integer.parseInt(br.readLine());
 		map = new int[N][N];
 		visited = new boolean[N][N];
@@ -26,50 +57,23 @@ public class Main2667 {
 				map[i][j] = s.charAt(j) - '0';
 			}
 		}
-
-		for (int i = 0; i < N; i++) {
-			for (int j = 0; j < N; j++) {
-				if(map[i][j] == 0 || visited[i][j]) continue;
-				cnt = 1;
-				DFS(i, j);
-				ans.add(cnt);
-			}
-		}
-
-		System.out.println(ans.size());
-		ans.sort(null);
-		for (Integer a : ans) {
-			System.out.println(a);
-		}
-
 	}
 
 	private static int DFS(int y, int x) {
 		visited[y][x] = true;
+		cnt++;
 		for (int i = 0; i < 4; i++) {
 			int ny = y + dy[i];
 			int nx = x + dx[i];
-			if(isMap(ny, nx) || visited[ny][nx] || map[ny][nx] == 0) continue;
+			if (isMap(ny, nx) || visited[ny][nx] || map[ny][nx] == 0)
+				continue;
 			visited[ny][nx] = true;
-			cnt++;
 			DFS(ny, nx);
 		}
 		return cnt;
 	}
 
-	//BFS
-
 	private static boolean isMap(int ny, int nx) {
 		return ny >= N || nx >= N || ny < 0 || nx < 0;
 	}
-
-	class Pair{
-		int y,x;
-
-		public Pair(int y, int x) {
-			this.y = y;
-			this.x = x;
-		}
-	}
-
 }
