853d9b4a91ae7390905d50ccd58426f450160449
sjk <80821744+ShinjaeKwon@users.noreply.github.com>
2022-10-18 18:02:47 +0900

    백준 3273

--- BaekJoon/Main3273.java
+++ BaekJoon/Main3273.java
@@ -1,37 +1,47 @@
-package Cho.week_3;
+package baekjoon.이분탐색;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
+class Main3273 {
 
-public class Main3273 {
+	static int N, X, ans;
+	static int[] a;
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int N = Integer.parseInt(br.readLine()); //수열의 갯수
-		int[] num = new int[N];
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		for(int i=0; i<N; i++){
-			num[i] = Integer.parseInt(st.nextToken());
-		}
-		Arrays.sort(num);
-		int X = Integer.parseInt(br.readLine()); //쌍의 합 
-		
-		int cnt = 0;
-		//1 2 3 5 7 9 10 11 12
-		for(int i=0; i<N-1; i++){
-			int a = X - num[i]; // X - num[i]값을 구한 후
-			for(int j=i+1; j<N; j++){
-				if(a == num[j]){ //num[i] 이후의 수열을 탐색, a값과 같으면 num[i]와 쌍 성립
-					cnt ++;
-					break;
-				}
-			}
-		}
-		System.out.println(cnt);
-		
+	public static void main(String[] args) throws Exception {
+		input();
+		go();
+		System.out.println(ans);
 	}
 
+	private static void go() {
+		int L = 0, R = N - 1;
+		while (L < R) {
+			if (a[L] + a[R] == X) {
+				ans++;
+				L++;
+				R--;
+			} else if (a[L] + a[R] > X) {
+				R--;
+			} else if (a[L] + a[R] < X) {
+				L++;
+			}
+		}
+	}
+
+	private static void input() throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		N = Integer.parseInt(br.readLine());
+		a = new int[N];
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < N; i++) {
+			a[i] = Integer.parseInt(st.nextToken());
+		}
+		X = Integer.parseInt(br.readLine());
+		Arrays.sort(a);
+	}
 }
