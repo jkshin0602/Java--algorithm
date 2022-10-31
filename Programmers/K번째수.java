d84de0aa1f29da97ef2bc16472434c1cf22c63c7
sjk <80821744+ShinjaeKwon@users.noreply.github.com>
2022-11-01 01:37:16 +0900

    프로그래머스 : K번째수

---
+++ Programmers/K번째수.java
@@ -0,0 +1,22 @@
+package programmers;
+
+import java.util.Arrays;
+
+public class K번째수 {
+
+	public static int[] solution(int[] array, int[][] commands) {
+		int[] answer = new int[commands.length];
+		for (int i = 0; i < commands.length; i++) {
+			int[] sliceArray = Arrays.copyOfRange(array, commands[i][0] - 1, commands[i][1]);
+			Arrays.sort(sliceArray);
+			answer[i] = sliceArray[commands[i][2] - 1];
+		}
+
+		return answer;
+	}
+
+	public static void main(String[] args) {
+		System.out.println(
+			Arrays.toString(solution(new int[] {1, 5, 2, 6, 3, 7, 4}, new int[][] {{2, 5, 3}, {4, 4, 1}, {1, 7, 3}})));
+	}
+}
