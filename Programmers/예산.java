d77cdca01ae8d8e7f5c43e892e564a377b8c6978
sjk <80821744+ShinjaeKwon@users.noreply.github.com>
2022-12-03 14:13:13 +0900

    프로그래머스 : 예산

--- Programmers/예산.java
+++ Programmers/예산.java
@@ -1,37 +1,37 @@
 package programmers;
 
-import java.util.*;
+import java.util.Arrays;
+import java.util.Comparator;
 
 public class 예산 {
 
 	public static int solution(int[] d, int budget) {
 		int answer = 0;
-		List<Integer> l = new ArrayList<>();
-		for (int i : d) {
-			l.add(i);
-		}
-		l.sort(Comparator.reverseOrder());
 
-		int c = 0;
-		while (c < l.size()) {
+		Integer[] numbersArray = Arrays.stream(d)
+			.boxed()
+			.toArray(Integer[]::new);
+		Arrays.sort(numbersArray, Comparator.reverseOrder());
+
+		for (int i = 0; i < numbersArray.length; i++) {
 			int tmp = budget;
 			int cnt = 0;
-			for (int i = c; i < l.size(); i++) {
-				tmp -= l.get(i);
+			for (int j = i; j < numbersArray.length; j++) {
+				tmp -= numbersArray[j];
 				if (tmp < 0) {
 					break;
-				} else {
-					cnt++;
 				}
+				cnt++;
 			}
 			answer = Math.max(answer, cnt);
-			c++;
 		}
+
 		return answer;
 	}
 
 	public static void main(String[] args) {
 		System.out.println(solution(new int[] {1, 3, 2, 5, 4}, 9));
 		System.out.println(solution(new int[] {2, 2, 3, 3}, 10));
+		System.out.println(solution(new int[] {120, 110, 140, 150}, 485));
 	}
 }
