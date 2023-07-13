baa1f8a651daa37eae74784b8d3abb7d2fa32262
sjk <sjk6437@gmail.com>
2023-07-13 13:50:48 +0900

    백준 2217

--- BaekJoon/Main2217.java
+++ BaekJoon/Main2217.java
@@ -1,25 +1,31 @@
-package baekjoon.그리디;
+package baekjoon.ready;
 
-import java.util.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
-class Main2217{
+class Main2217 {
+	//https://www.acmicpc.net/problem/2217
 
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(br.readLine());
-		PriorityQueue<Integer> pq = new PriorityQueue<>();
-		int max = -1;
-		for (int i = 0; i < N; i++) {
-			pq.add(Integer.parseInt(br.readLine()));
-		}
-		while (!pq.isEmpty()) {
-			max = Math.max(max, pq.poll() * (pq.size()+1));
-		}
-		System.out.println(max);
 
+		List<Integer> ropes = new ArrayList<>();
+		for (int i = 0; i < N; i++) {
+			ropes.add(Integer.parseInt(br.readLine()));
+		}
+
+		ropes.sort(Collections.reverseOrder());
+
+		int answer = 0;
+		for (int i = ropes.size() - 1; i >= 0; i--) {
+			int max = ropes.get(i) * (i + 1);
+			answer = Math.max(answer, max);
+		}
+
+		System.out.println(answer);
 	}
 }
-
-
-
