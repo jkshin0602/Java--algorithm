e24cbf5d71523bc13d639f2c1cfee84cc1f51ad2
sjk <sjk6437@gmail.com>
2023-07-21 16:03:38 +0900

    백준 16953

--- BaekJoon/Main16953.java
+++ BaekJoon/Main16953.java
@@ -1,4 +1,4 @@
-package baekjoon.그리디;
+package baekjoon.ready;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
@@ -6,44 +6,55 @@ import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
-public class Main16953 {
+class Main16953 {
 
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
+
 		int A = Integer.parseInt(st.nextToken());
 		int B = Integer.parseInt(st.nextToken());
-		Queue<Pair> q = new LinkedList<>();
 
-		q.add(new Pair(A, 1));
+		int answer = go(A, B);
+		System.out.println(answer);
+	}
+
+	private static int go(int a, int b) {
+		int count = -1;
+
+		Queue<Pair> q = new LinkedList<>();
+		q.add(new Pair(a, 1));
+
 		while (!q.isEmpty()) {
-			Pair tmp = q.poll();
-			if (tmp.a == B) {
-				System.out.println(tmp.c);
-				return;
+			Pair p = q.poll();
+
+			if (p.result == b) {
+				count = p.count;
+				break;
 			}
-			if (tmp.a > B) {
-				continue;
+
+			long multiply = p.result * 2;
+			long add = (p.result * 10) + 1;
+
+			if (multiply <= b) {
+				q.add(new Pair(multiply, p.count + 1));
 			}
-			if ((tmp.a * 2) <= B) {
-				q.add(new Pair(tmp.a * 2, tmp.c + 1));
-			}
-			if ((tmp.a * 10) + 1 <= B) {
-				q.add(new Pair((tmp.a * 10) + 1, tmp.c + 1));
+
+			if (add <= b) {
+				q.add(new Pair(add, p.count + 1));
 			}
 		}
-		System.out.println(-1);
+
+		return count;
 	}
 
 	private static class Pair {
-		long a;
-		int c;
+		long result;
+		int count;
 
-
-		public Pair(long a, int c) {
-			this.a = a;
-			this.c = c;
+		public Pair(long result, int count) {
+			this.result = result;
+			this.count = count;
 		}
 	}
-
 }
