c806a8777d7b837b843e368cb354a622426b15f3
sjk <sjk6437@gmail.com>
2023-07-28 15:39:12 +0900

    백준 17615

--- BaekJoon/Main17615.java
+++ BaekJoon/Main17615.java
@@ -1,74 +1,70 @@
+package baekjoon.ready;
 
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class Main17615 {
+class Main17615 {
 
-	public static void main(String[] args) throws IOException {
-		
+	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int N = Integer.parseInt(br.readLine()); 
-		String ball = br.readLine();
-		
+		int N = Integer.parseInt(br.readLine());
+		String s = br.readLine();
+
 		int red = 0;
 		int blue = 0;
-		// 1. 빨간 공과 파란 공의 개수를 세준다. 
-		for(int i=0; i<N; i++) {
-			if(ball.charAt(i) == 'R') {
+		for (int i = 0; i < N; i++) {
+			if (s.charAt(i) == 'R') {
 				red++;
-			}else{
+			} else {
 				blue++;
 			}
 		}
-		int min = Integer.MAX_VALUE;
 
-		// 2. 경우의 수는 4가지가 있다.
-		// 2-1. 빨강을 왼쪽, 오른쪽으로 다 넘기기 
-		// 2-2. 파랑을 왼쪽, 오른쪽으로 다 넘기기 
-		// 3. 넘겨야 하는 갯수는 어떤 색의 갯수에서 가장 끝에있는 갯수만 빼주면 된다.
-		// 4. 가장 끝에 있는 갯수를 연속해서 세준다. 
-		// 5. 모든 경우의 수에서 나온 가장 적은 이동횟수를 가진 것이 
-		int a = 0;
-		for(int i=0; i<N; i++) {
-			if(ball.charAt(i) == 'R') {
-				a++;
-			}else {
+		//왼쪽으로 빨간공 이동
+		int count = 0;
+		for (int i = 0; i < N; i++) {
+			if (s.charAt(i) == 'R') {
+				count++;
+			} else {
 				break;
 			}
 		}
-		min = red - a;
-		
-		a = 0;
-		for(int i=N-1; i>=0; i--) {
-			if(ball.charAt(i) == 'R') {
-				a++;
-			}else {
+		//전체 ball 갯수 - 왼쪽에 연속된 빨간공 개수 = 오른쪽에서 왼쪽으로 이동시킬 빨간공 개수
+		int min = red - count;
+
+		//오른쪽으로 빨간공 이동
+		count = 0;
+		for (int i = N - 1; i >= 0; i--) {
+			if (s.charAt(i) == 'R') {
+				count++;
+			} else {
 				break;
 			}
 		}
-		min = Math.min(min, red-a);
-		
-		a = 0;
-		for(int i=0; i<N; i++) {
-			if(ball.charAt(i) == 'B') {
-				a++;
-			}else {
+		min = Math.min(min, red - count);
+
+		//왼쪽으로 파란공 이동
+		count = 0;
+		for (int i = 0; i < N; i++) {
+			if (s.charAt(i) == 'B') {
+				count++;
+			} else {
 				break;
 			}
 		}
-		min = Math.min(min, blue-a);
-		
-		a = 0;
-		for(int i=N-1; i>=0; i--) {
-			if(ball.charAt(i) == 'B') {
-				a++;
-			}else {
+		min = Math.min(min, blue - count);
+
+		//오른쪽으로 파란공 이동
+		count = 0;
+		for (int i = N - 1; i >= 0; i--) {
+			if (s.charAt(i) == 'B') {
+				count++;
+			} else {
 				break;
 			}
 		}
-		min = Math.min(min, blue-a);
-		
+		min = Math.min(min, blue - count);
+
 		System.out.println(min);
 	}
 }
