294d688f160a9cce102a7da8d94198843dd90a44
sjk <sjk6437@gmail.com>
2023-07-29 14:40:30 +0900

    백준 11000

--- BaekJoon/Main11000.java
+++ BaekJoon/Main11000.java
@@ -1,50 +1,53 @@
-package baekjoon.그리디;
+package baekjoon.ready;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.PriorityQueue;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 class Main11000 {
 
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
 		int N = Integer.parseInt(br.readLine());
-		Pair[] a = new Pair[N];
 		PriorityQueue<Integer> pq = new PriorityQueue<>();
+		List<Pair> list = new ArrayList<>(N);
 
 		for (int i = 0; i < N; i++) {
-			st = new StringTokenizer(br.readLine());
-			a[i] = new Pair(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()));
-		}
-		Arrays.sort(a);
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int start = Integer.parseInt(st.nextToken());
+			int end = Integer.parseInt(st.nextToken());
 
-		pq.add(a[0].e);
+			list.add(new Pair(start, end));
+		}
+
+		Collections.sort(list);
+		pq.add(list.get(0).end);
+
 		for (int i = 1; i < N; i++) {
-			if (pq.peek() <= a[i].s) {
+			if (pq.peek() <= list.get(i).start) {
 				pq.poll();
 			}
-			pq.add(a[i].e);
+
+			pq.add(list.get(i).end);
 		}
+
 		System.out.println(pq.size());
 	}
 
 	private static class Pair implements Comparable<Pair> {
-		int s, e;
+		int start;
+		int end;
 
-		public Pair(int s, int e) {
-			this.s = s;
-			this.e = e;
+		public Pair(int start, int end) {
+			this.start = start;
+			this.end = end;
 		}
 
 		@Override
 		public int compareTo(Pair o) {
-			if (this.s != o.s) {
-				return this.s - o.s;
+			if (this.start == o.start) {
+				return this.end - o.end;
 			}
-			return this.e - o.e;
+			return this.start - o.start;
 		}
 	}
 }
