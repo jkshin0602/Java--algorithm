c72079bebe92edfe44bfc9f3f9b6afc98fc312e4
sjk <sjk6437@gmail.com>
2023-08-27 14:47:40 +0900

    백준 2309

--- BaekJoon/Main2309.java
+++ BaekJoon/Main2309.java
@@ -1,33 +1,35 @@
+package baekjoon.ready;
+
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 
-public class Main2309 {
+class Main2309 {
 
-	public static void main(String[] args) throws IOException{
+	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		int N = 9;
-		int[] A = new int[N];
+		int[] A = new int[9];
+
 		int sum = 0;
-		for(int i=0; i<N; i++){
+		for (int i = 0; i < 9; i++) {
 			A[i] = Integer.parseInt(br.readLine());
-			sum += A[i]; //9명의 키의 합
+			sum += A[i];
 		}
+
 		Arrays.sort(A);
 
-		for(int i=0; i<N; i++){
-			for(int j=i+1; j<N; j++){
-				if(sum - A[i] - A[j] == 100){
-					for(int k=0; k<N; k++){
-						if(j == k || i == k) continue;
+		for (int i = 0; i < 9; i++) {
+			for (int j = i + 1; j < 9; j++) {
+				if (sum - A[i] - A[j] == 100) {
+					for (int k = 0; k < 9; k++) {
+						if (k == i || k == j) {
+							continue;
+						}
 						System.out.println(A[k]);
 					}
-				 return;
+					return;
 				}
 			}
 		}
 	}
-
 }
